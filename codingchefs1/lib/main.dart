df31739c9f3bf7d8c331d28ba22b4dcd30413654
keepdev0919 <keepdev0919@gamil.com>
2025-07-29 23:43:30 +0900

    Snack bar와 BuildContext. 근데 5년전꺼라서 문법을 내가 변경한 부분 존재함

--- codingchefs1/lib/main.dart
+++ codingchefs1/lib/main.dart
@@ -31,89 +31,115 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appbar icon menu'),
+        title: Text('Snack Bar'),
         centerTitle: true,
-        elevation: 0.0,
-        // 앱바의 그림자 효과를 없앰 이라는데 솔직히 잘 모르겠음
-        actions: [
-          IconButton(
-            // actions는 앱바의 오른쪽에 위치하는 위젯
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('Shopping cart button is clicked');
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              print('Search button is clicked');
-            },
-          ),
-        ],
       ),
-      drawer: Drawer(
-          //drawer은 icon 설정안하고도 그냥 누르면 메뉴가 나옴
-          child: ListView(
-        padding: EdgeInsets.zero, // ListView의 기본 패딩을 제거
-        children: [
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/keepdev.jpg'),
-              backgroundColor: Colors.white,
+      body: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+          ),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('hi'),
+            ));
+          },
+          child: Text(
+            'Show me',
+            style: TextStyle(
+              color: Colors.white,
             ),
-            otherAccountsPictures: [
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/ChartDog.jpg'),
-                backgroundColor: Colors.white,
-              ),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/AiGrandFather.jpg'),
-                backgroundColor: Colors.white,
-              ),
-            ],
-            accountName: Text('KeepDev'),
-            accountEmail: Text('keepdev0919@gmail.com'),
-            onDetailsPressed: () {
-              // onDetailsPressed는 계정 정보 클릭시 이벤트
-              print('Details pressed');
-            },
-            // decoration: BoxDecoration(
-            //   //userAccountsDrawerHeader의 배경색 설정
-            //   color: Colors.red[800],
-            //   borderRadius: BorderRadius.only(
-            //     topLeft: Radius.circular(40.0),
-            //     bottomLeft: Radius.circular(40.0),
-            //     bottomRight: Radius.circular(40.0),
-            //   ),
-            // ),
           ),
-          ListTile(
-            leading: Icon(Icons.home, color: Colors.grey[850]),
-            title: Text('Home'),
-            onTap: () {
-              print('Home is clicked');
-              // Navigator.pop(context); // 메뉴를 닫음
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-            leading: Icon(Icons.home, color: Colors.grey[850]),
-            title: Text('Setting'),
-            onTap: () {
-              print('Setting is clicked');
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-            leading: Icon(Icons.question_answer),
-            title: Text('Q&A'),
-            onTap: () {
-              print('Q&A is clicked');
-            },
-            trailing: Icon(Icons.add),
-          ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Appbar icon menu'),
+    //     centerTitle: true,
+    //     elevation: 0.0,
+    //     // 앱바의 그림자 효과를 없앰 이라는데 솔직히 잘 모르겠음
+    //     actions: [
+    //       IconButton(
+    //         // actions는 앱바의 오른쪽에 위치하는 위젯
+    //         icon: const Icon(Icons.shopping_cart),
+    //         onPressed: () {
+    //           print('Shopping cart button is clicked');
+    //         },
+    //       ),
+    //       IconButton(
+    //         icon: const Icon(Icons.search),
+    //         onPressed: () {
+    //           print('Search button is clicked');
+    //         },
+    //       ),
+    //     ],
+    //   ),
+    //   drawer: Drawer(
+    //       //drawer은 icon 설정안하고도 그냥 누르면 메뉴가 나옴
+    //       child: ListView(
+    //     padding: EdgeInsets.zero, // ListView의 기본 패딩을 제거
+    //     children: [
+    //       UserAccountsDrawerHeader(
+    //         currentAccountPicture: CircleAvatar(
+    //           backgroundImage: AssetImage('assets/images/keepdev.jpg'),
+    //           backgroundColor: Colors.white,
+    //         ),
+    //         otherAccountsPictures: [
+    //           CircleAvatar(
+    //             backgroundImage: AssetImage('assets/images/ChartDog.jpg'),
+    //             backgroundColor: Colors.white,
+    //           ),
+    //           CircleAvatar(
+    //             backgroundImage: AssetImage('assets/images/AiGrandFather.jpg'),
+    //             backgroundColor: Colors.white,
+    //           ),
+    //         ],
+    //         accountName: Text('KeepDev'),
+    //         accountEmail: Text('keepdev0919@gmail.com'),
+    //         onDetailsPressed: () {
+    //           // onDetailsPressed는 계정 정보 클릭시 이벤트
+    //           print('Details pressed');
+    //         },
+    //         // decoration: BoxDecoration(
+    //         //   //userAccountsDrawerHeader의 배경색 설정
+    //         //   color: Colors.red[800],
+    //         //   borderRadius: BorderRadius.only(
+    //         //     topLeft: Radius.circular(40.0),
+    //         //     bottomLeft: Radius.circular(40.0),
+    //         //     bottomRight: Radius.circular(40.0),
+    //         //   ),
+    //         // ),
+    //       ),
+    //       ListTile(
+    //         leading: Icon(Icons.home, color: Colors.grey[850]),
+    //         title: Text('Home'),
+    //         onTap: () {
+    //           print('Home is clicked');
+    //           // Navigator.pop(context); // 메뉴를 닫음
+    //         },
+    //         trailing: Icon(Icons.add),
+    //       ),
+    //       ListTile(
+    //         leading: Icon(Icons.home, color: Colors.grey[850]),
+    //         title: Text('Setting'),
+    //         onTap: () {
+    //           print('Setting is clicked');
+    //         },
+    //         trailing: Icon(Icons.add),
+    //       ),
+    //       ListTile(
+    //         leading: Icon(Icons.question_answer),
+    //         title: Text('Q&A'),
+    //         onTap: () {
+    //           print('Q&A is clicked');
+    //         },
+    //         trailing: Icon(Icons.add),
+    //       ),
+    //     ],
+    //   )),
+    // );
+ 
