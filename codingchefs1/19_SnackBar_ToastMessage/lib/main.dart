91dce96b8d33770668ffbcc16feddf2695ce0a20
keepdev0919 <keepdev0919@gamil.com>
2025-07-30 11:08:46 +0900

    폴더구조 변경

---
+++ codingchefs1/19_SnackBar_ToastMessage/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp as Widget);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
--- codingchefs1/android/app/src/main/kotlin/com/example/codingchefs1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.codingchefs1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
